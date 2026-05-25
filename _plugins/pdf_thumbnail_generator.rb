c04987b52ae9ac6417ed68cf12567645c2536dd1
Anirudh Gupta <anirudhgupta@iisc.ac.in>
2026-05-25 15:21:39 +0530

    clubs, thumbnails

---
+++ _plugins/pdf_thumbnail_generator.rb
@@ -0,0 +1,16 @@
+require 'fileutils'
+
+clubs_dir = File.join('assets', 'clubs')
+edition_dir = File.join('PreviousEditions')
+
+[clubs_dir, edition_dir].each do |base_dir|
+  Dir.glob(File.join(base_dir, '**', '*.pdf')).each do |pdf_path|
+    thumb_path = pdf_path.sub(/\.pdf$/i, '.webp')
+    next if File.exist?(thumb_path) && File.mtime(thumb_path) >= File.mtime(pdf_path)
+
+    success = system("magick", "-density", "150", "#{pdf_path}[0]", "-thumbnail", "600x",
+                     "-background", "white", "-alpha", "remove", "-quality", "85", thumb_path)
+    puts success ? "  ✓ #{File.basename(pdf_path)}" : "  ✗ #{File.basename(pdf_path)}"
+  end
+end
+
